03fb91eedd85756acbe202cdd6df8043062784f8
viell <viell.dev@gmail.com>
2023-08-28 15:56:21 +0200

    Refactored 2015 day 1 and 2

--- events/2015/day01/src/main.rs
+++ events/2015/day01/src/main.rs
@@ -1,62 +1,72 @@
-fn get_awnsers(input: &str) -> (i32, i32) {
-    let mut floor: i32 = 0;
-    let mut basement: i32 = 0;
+//! Day 1: Not Quite Lisp
 
+/// Print the answers for the puzzle to the console.
+fn main() {
+    let input = include_str!("input.txt").trim();
+    let (floor, basement) = get_answers(input);
+
+    println!("The instructions took Santa to floor {}.", floor);
+    println!(
+        "The character that caused Santa to first {} {}.",
+        "enter the basement is at position", basement
+    );
+}
+
+/// Get the answers for the puzzle.
+fn get_answers(input: &str) -> (isize, isize) {
+    // Current floor.
+    let mut floor: isize = 0;
+    // Position of first basement entry.
+    let mut basement: isize = 0;
+
+    // Iterate over each character in the input.
     for (i, c) in input.chars().enumerate() {
+        // Update the floor based on the current character.
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
             _ => (),
         }
 
+        // If this is the first basement entry, record the position.
         if basement == 0 && floor == -1 {
-            basement = i as i32 + 1;
+            basement = i as isize + 1;
         }
     }
 
+    // Return the answers.
     (floor, basement)
-}
-
-fn main() {
-    let input = include_str!("input.txt").trim();
-
-    let (floor, basement) = get_awnsers(input);
-
-    if basement == 0 {
-        println!("Santa never entered the basement");
-    } else {
-        println!("Santa first entered the basement at position {}", basement);
-    }
-
-    println!("Santa ended up on floor {}", floor);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    /// Test the examples given for part 1 on the puzzle page.
     #[test]
-    fn test_part1() {
-        assert_eq!(get_awnsers("(())").0, 0);
-        assert_eq!(get_awnsers("()()").0, 0);
-        assert_eq!(get_awnsers("(((").0, 3);
-        assert_eq!(get_awnsers("(()(()(").0, 3);
-        assert_eq!(get_awnsers("))(((((").0, 3);
-        assert_eq!(get_awnsers("())").0, -1);
-        assert_eq!(get_awnsers("))(").0, -1);
-        assert_eq!(get_awnsers(")))").0, -3);
-        assert_eq!(get_awnsers(")())())").0, -3);
+    fn test_part1_examples() {
+        assert_eq!(get_answers("(())").0, 0);
+        assert_eq!(get_answers("()()").0, 0);
+        assert_eq!(get_answers("(((").0, 3);
+        assert_eq!(get_answers("(()(()(").0, 3);
+        assert_eq!(get_answers("))(((((").0, 3);
+        assert_eq!(get_answers("())").0, -1);
+        assert_eq!(get_answers("))(").0, -1);
+        assert_eq!(get_answers(")))").0, -3);
+        assert_eq!(get_answers(")())())").0, -3);
     }
 
+    /// Test the examples given for part 2 on the puzzle page.
     #[test]
-    fn test_part2() {
-        assert_eq!(get_awnsers(")").1, 1);
-        assert_eq!(get_awnsers("()())").1, 5);
+    fn test_part2_examples() {
+        assert_eq!(get_answers(")").1, 1);
+        assert_eq!(get_answers("()())").1, 5);
     }
 
+    /// Test the answers against the correct answers.
     #[test]
-    fn test_awnsers() {
+    fn test_answers() {
         let input = include_str!("input.txt").trim();
-        assert_eq!(get_awnsers(input), (232, 1783));
+        assert_eq!(get_answers(input), (232, 1783));
     }
 }
--- events/2015/day02/src/main.rs
+++ events/2015/day02/src/main.rs
@@ -1,102 +1,89 @@
-#[derive(Debug, PartialEq)]
-enum ParseError {
-    ParseIntError,
-    TooManyParts,
-    TooFewParts,
-}
+//! Day 2: I Was Told There Would Be No Math
 
-fn get_awnsers(input: &str) -> Result<(i32, i32), ParseError> {
-    let mut lwh = Vec::<i32>::with_capacity(3);
-
-    for part in input.split("x") {
-        if lwh.len() == 3 {
-            return Err(ParseError::TooManyParts);
-        }
-
-        match part.parse::<i32>() {
-            Ok(i) => lwh.push(i),
-            Err(_) => return Err(ParseError::ParseIntError),
-        }
-    }
-
-    if lwh.len() != 3 {
-        return Err(ParseError::TooFewParts);
-    }
-
-    lwh.sort();
-
-    let (l, w, h) = (lwh[0], lwh[1], lwh[2]);
-
-    let (lw, wh, hl) = (l * w, w * h, h * l);
-
-    let area = 3 * lw + 2 * wh + 2 * hl;
-
-    let ribbon = 2 * l + 2 * w + l * w * h;
-
-    Ok((area, ribbon))
-}
-
+/// Print the answers for the puzzle to the console.
 fn main() {
     let input = include_str!("input.txt").trim();
-    let mut total_area = 0;
-    let mut total_ribbon = 0;
-
-    for line in input.lines() {
-        match get_awnsers(line) {
-            Ok((area, ribbon)) => {
-                total_area += area;
-                total_ribbon += ribbon;
-            }
-            Err(e) => println!("Error: {:?}", e),
-        }
-    }
+    let (total_area, total_ribbon) = get_answers(input);
 
     println!("Total area: {}", total_area);
     println!("Total ribbon: {}", total_ribbon);
+}
+
+/// Get the answers for the puzzle.
+fn get_answers(input: &str) -> (usize, usize) {
+    // Square feet of wrapping paper
+    let mut paper: usize = 0;
+    // Feet of ribbon
+    let mut ribbon: usize = 0;
+
+    // Loop over each line in the input
+    for line in input.lines() {
+        // Get the dimensions.
+        let (l, w, h) = get_dimensions(line);
+        // Calculate the areas of each side. `lw` will be the smallest side.
+        let (lw, wh, hl) = (l * w, w * h, h * l);
+        // Calculate the total area plus the area of the smallest side.
+        let area = 3 * lw + 2 * wh + 2 * hl;
+        // Calculate the length of the ribbon and bow.
+        let length = 2 * l + 2 * w + l * w * h;
+
+        // Add the areas and lengths to the totals.
+        paper += area;
+        ribbon += length;
+    }
+
+    // Return the answers
+    (paper, ribbon)
+}
+
+/// Get the dimensions from a line of input sorted from smallest to largest.
+fn get_dimensions(line: &str) -> (usize, usize, usize) {
+    // Extract the dimensions from the line and parse them into integers
+    let mut dimensions = line
+        .split("x")
+        .map(|s| match s.parse::<usize>() {
+            Ok(i) => i,
+            // If the parsing failed, panic
+            Err(_) => panic!("Invalid input"),
+        })
+        .collect::<Vec<_>>();
+
+    // Sort the dimensions so that the smallest is first
+    dimensions.sort();
+
+    // Destructure the dimensions into `l`, `w`, and `h`
+    if let [l, w, h] = dimensions.as_slice() {
+        // Return the dimensions
+        return (*l, *w, *h);
+    }
+
+    // If the destructuring failed, panic
+    panic!("Invalid input");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    /// Test the examples given for part 1 on the puzzle page.
     #[test]
-    fn test_part1() {
-        assert_eq!(get_awnsers("2x3x4").unwrap().0, 58);
-        assert_eq!(get_awnsers("1x1x10").unwrap().0, 43);
+    fn test_part1_examples() {
+        assert_eq!(get_answers("2x3x4").0, 58);
+        assert_eq!(get_answers("1x1x10").0, 43);
     }
 
+    /// Test the examples given for part 2 on the puzzle page.
     #[test]
-    fn test_part2() {
-        assert_eq!(get_awnsers("2x3x4").unwrap().1, 34);
-        assert_eq!(get_awnsers("1x1x10").unwrap().1, 14);
+    fn test_part2_examples() {
+        assert_eq!(get_answers("2x3x4").1, 34);
+        assert_eq!(get_answers("1x1x10").1, 14);
     }
 
-    #[test]
-    fn test_error() {
-        assert_eq!(get_awnsers("axbxc").unwrap_err(), ParseError::ParseIntError);
-        assert_eq!(
-            get_awnsers("2x3x4x5").unwrap_err(),
-            ParseError::TooManyParts
-        );
-        assert_eq!(get_awnsers("2x3").unwrap_err(), ParseError::TooFewParts);
-    }
-
+    /// Test the answers against the correct answers.
     #[test]
     fn test_awnsers() {
         let input = include_str!("input.txt").trim();
-        let mut total_area = 0;
-        let mut total_ribbon = 0;
-
-        for line in input.lines() {
-            match get_awnsers(line) {
-                Ok((area, ribbon)) => {
-                    total_area += area;
-                    total_ribbon += ribbon;
-                }
-                Err(e) => println!("Error: {:?}", e),
-            }
-        }
-
+        let (total_area, total_ribbon) = get_answers(input);
         assert_eq!((total_area, total_ribbon), (1586300, 3737498));
     }
 }
