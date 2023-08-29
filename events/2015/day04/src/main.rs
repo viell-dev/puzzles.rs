ec74ed7a0f527f577d8270a400bb0815abe80e3f
viell <viell.dev@gmail.com>
2023-08-29 13:53:33 +0200

    refactored 2015 day04

--- events/2015/day04/src/main.rs
+++ events/2015/day04/src/main.rs
@@ -1,39 +1,61 @@
+//! Day 4: The Ideal Stocking Stuffer
+
 use md5;
 
-fn get_awnsers(input: &str) -> (i32, i32) {
+/// Print the answers for the puzzle to the console.
+fn main() {
+    let input = include_str!("input.txt").trim();
+    let (five_zeros, six_zeros) = get_answers(input);
+
+    println!(
+        "Lowest possible number resulting in an MD5 hash {}: {}",
+        "starting with five zeros is", five_zeros
+    );
+    println!(
+        "Lowest possible number resulting in an MD5 hash {}: {}",
+        "starting with six zeros is", six_zeros
+    );
+}
+
+fn get_answers(input: &str) -> (usize, usize) {
+    // Initialize variables to hold the answers.
     let mut five_zeros = 0;
     let mut six_zeros = 0;
 
+    // Loop through all numbers starting at 0.
     for i in 0.. {
         let hash = md5::compute(format!("{}{}", input, i));
         let hash = format!("{:x}", hash);
 
-        if six_zeros == 0 && hash.starts_with("000000") {
+        /* Check if the hash starts with six zeros
+        and six_zeros hasn't been set. */
+        if hash.starts_with("000000") && six_zeros == 0 {
+            // Set six_zeros to i.
             six_zeros = i;
-        }
-        if five_zeros == 0 && hash.starts_with("00000") {
-            five_zeros = i;
-        }
 
-        if six_zeros != 0 && five_zeros != 0 {
+            if five_zeros == 0 {
+                // Set five_zeros to i.
+                five_zeros = i;
+            }
+
+            // Break since both five_zeros and six_zeros have been set.
             break;
+        }
+        /* Check if the hash starts with five zeros
+        and five_zeros hasn't been set. */
+        else if hash.starts_with("00000") && five_zeros == 0 {
+            // Set five_zeros to i.
+            five_zeros = i;
+
+            // Break if both five_zeros and six_zeros have been set.
+            if six_zeros != 0 {
+                break;
+            }
         }
     }
 
+    // Return the answers.
     (five_zeros, six_zeros)
-}
-
-fn main() {
-    let input = include_str!("input.txt").trim();
-    let (five_zeros, six_zeros) = get_awnsers(input);
-    println!(
-        "Lowest possible number producing MD5 starting with 5 zeros is: {}",
-        five_zeros
-    );
-    println!(
-        "Lowest possible number producing MD5 starting with 6 zeros is: {}",
-        six_zeros
-    );
 }
 
 #[cfg(test)]
@@ -41,14 +63,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1() {
-        assert_eq!(get_awnsers("abcdef").0, 609043);
-        assert_eq!(get_awnsers("pqrstuv").0, 1048970);
+    fn test_part1_examples() {
+        assert_eq!(get_answers("abcdef").0, 609043);
+        assert_eq!(get_answers("pqrstuv").0, 1048970);
     }
 
+    /// Test the answers against the correct answers.
     #[test]
-    fn test_awnsers() {
+    fn test_answers() {
         let input = include_str!("input.txt").trim();
-        assert_eq!(get_awnsers(input), (346386, 9958218));
+        assert_eq!(get_answers(input), (346386, 9958218));
     }
 }
