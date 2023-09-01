97de181d42c2b754b0b1ed998c411ff2b0e31fb4
viell <viell.dev@gmail.com>
2023-09-01 08:35:23 +0200

    removed todo!()'s from example main.rs to prevent cargo test from failing in new projects

--- example/main.rs
+++ example/main.rs
@@ -7,13 +7,11 @@ fn main() {
 
     println!("a is: {}", a);
     println!("b is: {}", b);
-    todo!();
 }
 
 /// Get the answers for the puzzle.
 fn get_answers(input: &str) -> (usize, usize) {
     println!("{}", input);
-    todo!();
 }
 
 #[cfg(test)]
@@ -22,21 +20,16 @@ mod tests {
 
     /// Test the examples given for part 1 on the puzzle page.
     #[test]
-    fn test_part1_examples() {
-        todo!();
-    }
+    fn test_part1_examples() {}
 
     /// Test the examples given for part 2 on the puzzle page.
     #[test]
-    fn test_part2_examples() {
-        todo!();
-    }
+    fn test_part2_examples() {}
 
     /// Test the answers against the correct answers.
     #[test]
     fn test_answers() {
         let input = include_str!("input.txt").trim();
         assert_eq!(get_answers(input), (0, 0));
-        todo!();
     }
 }
